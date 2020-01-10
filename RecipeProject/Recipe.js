af021ff94849db346a17e86de40fbf8fd0ab6e00
GrunuffyGit <Gloria.wm.tam@gmail.com>
2020-01-10 15:13:27 -0800

    adding recipe project

---
+++ RecipeProject/Recipe.js
@@ -0,0 +1,13 @@
+var curryImageElement = document.getElementById("curryPic");
+console.log(curryImageElement);
+var curryImagesURL = ["decorativecurryMed.png","decorativecurrySmall.png","decorativecurryLarge.png"];
+var x=0;
+function changingCurry(){
+    curryImageElement.setAttribute("src", curryImagesURL[x]);
+    x=x+1;
+    if(x==3){
+    x=0;
+    }
+}
+
+setInterval(function(){changingCurry();}, 2500);
