5e7604e99cb429f3f0d44b0ca4e208a9ea1d8d8e
GrunuffyGit <Gloria.wm.tam@gmail.com>
2020-01-13 14:17:20 -0800

    added dropdown menu

--- Assignments/RecipeProject/JS/Recipe.js
+++ Assignments/RecipeProject/JS/Recipe.js
@@ -1,19 +1,33 @@
-var x=0;
 var curryImageElement = document.getElementById("curryPic");
+var menuButton = document.getElementById("dropDownBtn");
+var menuList = document.getElementById("dropDownList");
 var curryImagesURL;
-function getCurryName(){
-    var curryName = document.getElementById("curryName").innerHTML.replace(/\s+/g, '');
-    curryName = curryName.toLowerCase();
+var curryImageIndexNum = 0;
+var displayMenuOnOff = false;
+
+function getCurryImageNameArray(){
+    var curryName = document.getElementById("curryName").innerHTML.replace(/\s+/g, '').toLowerCase();
     curryImagesURL = [curryName+"Med.png", curryName+"Small.png", curryName+"Large.png"];
 }
 
 function changingCurry(){
-    curryImageElement.setAttribute("src", "../Images/"+curryImagesURL[x]);
-    x=x+1;
-    if(x==3){
-    x=0;
+    curryImageElement.setAttribute("src", "../Images/"+curryImagesURL[curryImageIndexNum]);
+    curryImageIndexNum = curryImageIndexNum+1;
+    if(curryImageIndexNum === 3){
+    curryImageIndexNum = 0;
     }
 }
 
-getCurryName();
-setInterval(function(){changingCurry();}, 2500);
+function displayingMenu(){
+    if(displayMenuOnOff === true){
+        menuList.style.display = "none";
+        displayMenuOnOff = false;
+    }else{
+        menuList.style.display = "block";
+        displayMenuOnOff = true;
+    }
+}
+
+menuButton.onclick = function(){displayingMenu()};
+getCurryImageNameArray();
+setInterval(function(){changingCurry();}, 2000);
