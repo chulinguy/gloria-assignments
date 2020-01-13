f6c16018da92177ed294bc4fb1561ce1c7bf1620
GrunuffyGit <Gloria.wm.tam@gmail.com>
2020-01-13 09:51:09 -0800

    reorganized files into folders

--- Assignments/RecipeProject/JS/Recipe.js
+++ Assignments/RecipeProject/JS/Recipe.js
@@ -3,7 +3,7 @@ console.log(curryImageElement);
 var curryImagesURL = ["decorativecurryMed.png","decorativecurrySmall.png","decorativecurryLarge.png"];
 var x=0;
 function changingCurry(){
-    curryImageElement.setAttribute("src", curryImagesURL[x]);
+    curryImageElement.setAttribute("src", "../Images/"+curryImagesURL[x]);
     x=x+1;
     if(x==3){
     x=0;
