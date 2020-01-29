25b07ea45a93307fa96023a090985e7d4183480a
GrunuffyGit <Gloria.wm.tam@gmail.com>
2020-01-28 17:30:45 -0800

    completed coing game

---
+++ Assignments/Game/coinFlip.js
@@ -0,0 +1,371 @@
+let kirbyBody=document.getElementById("kirby");
+let kirbyRArm=document.getElementById("arm-right");
+let kirbyLArm=document.getElementById("arm-left");
+let kirbyRLeg=document.getElementById("leg-right");
+let kirbyLLeg=document.getElementById("leg-left");
+let kirbyREye = document.getElementById("kirby-eye-right");
+let kirbyLEye = document.getElementById("kirby-eye-left");
+let closedREye = document.getElementById("close-eye-right");
+let closedLEye = document.getElementById("close-eye-left");
+let coinBox = document.getElementById("coinBox");
+let coinHT = document.getElementById("coinHT");
+let coinArray = [];
+
+function flipCoin(){
+    //turning off game select modal
+    //entering mode's array into the generator
+    if(event.target.id === "inputBtn"){
+        let coinFlipVal = document.getElementById("coinsToFlip").value;
+        if(checkingflipNum(coinFlipVal)){
+            document.addEventListener("keyup",function(){hitCoinBox(coinArray, coinFlipVal)});
+            document.addEventListener("keydown",kirbyStartJumpPosition);
+        }
+    }
+    
+}
+
+function checkingflipNum(coinFlipInput){
+    //making sure user inputed value is a number greater than 0
+    let errorMsging = document.getElementById("errorMsg");
+    if(!isNaN(Number(coinFlipInput))){
+        //if coinflipInput is not a NaN
+        if(coinFlipInput>0&& coinFlipInput<51){
+            let modal = document.getElementsByClassName("gameModal");
+            modal[0].style.display = "none";
+            return true;
+        }else if(coinFlipInput>=51){
+            errorMsging.innerHTML = "Too many coins! Max is 50 please.";
+            return false;
+        }else{
+            errorMsging.innerHTML = "Please enter a positive number!";
+            return false;
+        }
+    }else{
+        errorMsging.innerHTML = "Please enter a number!"
+        return false;
+    }
+}
+
+function moveKirbyBody(duration){
+    //stationary movement of the body
+    kirbyBody.animate(
+        [
+            {transform:'translateY(0vh)', offset: 0},
+            {transform:'translateY(-2vh)', offset: .4},
+            {transform:'translateY(-2vh)', offset: .5},
+            {transform:'translateY(0vh)', offset: .95},
+            {transform:'translateY(0vh)', offset: 1}
+        ],
+        {
+            duration: duration,
+            iterations: Infinity,
+            easing: 'linear'
+        }
+    );
+}
+
+function moveKirbyLegs(duration){
+    //stationary movement of the legs
+    kirbyRLeg.animate(
+        [
+            {transform:'rotate(0deg)', offset: 0},
+            {transform:'rotate(65deg)', offset: .4},
+            {transform:'rotate(65deg)', offset: .5},
+            {transform:'rotate(0deg)', offset: .95},
+            {transform:'rotate(0deg)', offset: 1}
+        ],
+        {
+            duration: duration,
+            iterations: Infinity,
+            easing: 'linear'
+        }
+    );
+    kirbyLLeg.animate(
+        [
+            {transform:'rotate(0deg)', offset: 0},
+            {transform:'rotate(-75deg)', offset: .4},
+            {transform:'rotate(-75deg)', offset: .5},
+            {transform:'rotate(0deg)', offset: .95},
+            {transform:'rotate(0deg)', offset: 1}
+        ],
+        {
+            duration: duration,
+            iterations: Infinity,
+            easing: 'linear'
+        }
+    );
+}
+
+function moveKirbyArms(duration){
+    //stationary movement of the arms
+    kirbyRArm.animate(
+        [
+            {transform:'rotate(0deg)', offset: 0},
+            {transform:'rotate(20deg) translate(-.5vw,-1vh)', offset: .4},
+            {transform:'rotate(20deg) translate(-.5vw,-1vh)', offset: .5},
+            {transform:'rotate(0deg)', offset: .95},
+            {transform:'rotate(0deg)', offset: 1}
+        ],
+        {
+            duration: duration,
+            iterations: Infinity,
+            easing: 'linear'
+        }
+    );
+    kirbyLArm.animate(
+        [
+            {transform:'rotate(0deg)', offset: 0},
+            {transform:'rotate(-20deg) translate(.5vw, -1vh)', offset: .4},
+            {transform:'rotate(-20deg) translate(.5vw, -1vh)', offset: .5},
+            {transform:'rotate(0deg)', offset: .95},
+            {transform:'rotate(0deg)', offset: 1}
+        ],
+        {
+            duration: duration,
+            iterations: Infinity,
+            easing: 'linear'
+        }
+    );
+}
+
+function kirbyBlink(){
+    //blinking movements
+    closeEyes(kirbyREye,kirbyLEye,closedLEye,closedREye);
+    setTimeout(function(){openEyes(kirbyREye,kirbyLEye,closedLEye,closedREye);}, 100);
+    
+}
+
+function closeEyes(REye,LEye,closeLEye,closeREye){
+    //closing the eyes
+    REye.style.display = "none";
+    LEye.style.display = "none";
+    closeLEye.style.display = "block";
+    closeREye.style.display = "block";
+}
+
+function openEyes(REye,LEye,closeLEye,closeREye){
+    //opening the eyes
+    REye.style.display = "block";
+    LEye.style.display = "block";
+    closeLEye.style.display = "none";
+    closeREye.style.display = "none";
+}
+
+function kirbyMotion(){
+    //all stationary movements for the entire character
+    moveKirbyArms(2000);
+    moveKirbyBody(2000);
+    moveKirbyLegs(2000);
+    setInterval(function(){kirbyBlink();}, 2000);
+}
+
+function kirbyStartJumpPosition(){
+    //starting position of the jump
+    let duration = 500;
+    closeEyes(kirbyREye,kirbyLEye,closedLEye,closedREye);
+    kirbyBody.animate(
+        [
+            {transform:'scaleY(.55) translateY(6vh)', offset: 0},
+            {transform:'scaleY(.55) translateY(6vh)', offset: 1}
+        ],
+        {
+            duration: duration,
+            easing: 'linear'
+        }
+    );
+    kirbyRArm.animate(
+        [
+            {transform:'scaleY(.8) translateY(3.5vh)', offset: 0},
+            {transform:'scaleY(.8) translateY(3.5vh)', offset: 1}
+        ],
+        {
+            duration: duration,
+            easing: 'linear'
+        }
+    );
+    kirbyLArm.animate(
+        [
+            {transform:'scaleY(.8) translateY(3.5vh)', offset: 0},
+            {transform:'scaleY(.8) translateY(3.5vh)', offset: 1}
+        ],
+        {
+            duration: duration,
+            easing: 'linear'
+        }
+    );
+    kirbyRLeg.animate(
+        [
+            {transform:'rotate(0deg)', offset: 0},
+            {transform:'rotate(0deg)', offset: 1}
+        ],
+        {
+            duration: duration,
+            easing: 'linear'
+        }
+    );
+    kirbyLLeg.animate(
+        [
+            {transform:'rotate(0deg)', offset: 0},
+            {transform:'rotate(0deg)', offset: 1}
+        ],
+        {
+            duration: duration,
+            easing: 'linear'
+        }
+    );
+}
+
+function kirbyJumping(duration){
+    //full body jumping and hitting th block
+    kirbyBody.animate(
+        [
+            {transform:'scaleY(1) translateY(0vh)', offset: 0},
+            {transform:'scaleY(.8) translateY(-35vh)', offset: .3},
+            {transform:'scaleY(.8) translateY(-42vh)', offset: .5},
+            {transform:'scaleY(1) translateY(-28vh)', offset: .7},
+            {transform:'scaleY(1) translateY(0vh)', offset: 1}
+        ],
+        {
+            duration: duration,
+            easing: 'linear'
+        }
+    );
+    kirbyRArm.animate(
+        [
+            {transform:'scaleY(.8) translateY(2vh)', offset: 0},
+            {transform:'scaleY(.8) translateY(-35vh)', offset: .3},
+            {transform:'scaleY(1) translateY(-33vh)', offset: .5},
+            {transform:'scaleY(1) translateY(-30vh)', offset: .7},
+            {transform:'scaleY(.8) translateY(2vh)', offset: 1}
+        ],
+        {
+            duration: duration,
+            easing: 'linear'
+        }
+    );
+    kirbyLArm.animate(
+        [
+            {transform:'scaleY(.8) translateY(2vh)', offset: 0},
+            {transform:'scaleY(.8) translateY(-35vh)', offset: .3},
+            {transform:'scaleY(1) translateY(-33vh)', offset: .5},
+            {transform:'scaleY(1) translateY(-30vh)', offset: .7},
+            {transform:'scaleY(.8) translateY(2vh)', offset: 1}
+        ],
+        {
+            duration: duration,
+            easing: 'linear'
+        }
+    );
+    kirbyRLeg.animate(
+        [
+            {transform:'rotate(90deg) translateX(0vh)', offset: 0},
+            {transform:'rotate(90deg) translateX(-30vh)', offset: .3},
+            {transform:'rotate(90deg) translateX(-35vh)', offset: .5},
+            {transform:'rotate(90deg) translateX(-30vh)', offset: .7},
+            {transform:'rotate(90deg) translateX(0vh)', offset: 1}
+        ],
+        {
+            duration: duration,
+            easing: 'linear'
+        }
+    );
+    kirbyLLeg.animate(
+        [
+            {transform:'rotate(-90deg) translateX(0vh)', offset: 0},
+            {transform:'rotate(-90deg) translateX(30vh)', offset: .3},
+            {transform:'rotate(-90deg) translateX(35vh)', offset: .5},
+            {transform:'rotate(-90deg) translateX(30vh)', offset: .7},
+            {transform:'rotate(-90deg) translateX(0vh)', offset: 1}
+        ],
+        {
+            duration: duration,
+            easing: 'linear'
+        }
+    );
+}
+
+function moveCoinBox(duration){
+    coinBox.animate([
+        {transform:'translateY(0vh)', offset: 0},
+        {transform:'translateY(0vh)', offset: .25},
+        {transform:'translateY(-6vh)', offset: .4},
+        {transform:'translateY(-9vh)', offset: .7},
+        {transform:'translateY(0vh)', offset: 1}
+    ],
+    {
+        duration: duration,
+        easing: 'linear'
+    });
+}
+
+function moveCoin(duration){
+    coinHT.animate([
+        {opacity: 0, transform:'translateY(0vh) rotateY(0deg)', offset: 0},
+        {opacity: 0, transform:'translateY(0vh) rotateY(0deg)', offset: .075},
+        {opacity: 0, transform:'translateY(-12vh) rotateY(360deg)', offset: .099},
+        {opacity: 1, transform:'translateY(-20vh) rotateY(360deg)', offset: .1},
+        {opacity: 1, transform:'translateY(-35vh) rotateY(0deg)', offset: .15},
+        {opacity: 1, transform:'translateY(-35vh) rotateY(360deg)', offset: .175},
+        {opacity: 1, transform:'translateY(-12vh) rotateY(0deg)', offset: .25},
+        {opacity: 1, transform:'translateY(-12vh) rotateY(360deg)', offset: 1}
+    ],
+    {
+        duration: duration,
+        easing: 'linear'
+    });
+}
+
+function headsOrTails(){
+    //generating head or tails
+    let chances = Math.random();
+    if (chances < .5){
+        return "heads";
+    }else{
+        return "tails";
+    }
+}
+
+function hitCoinBox(array, arrayMaxLength){
+    if(array.length<arrayMaxLength){
+        let coinToSpin = headsOrTails();
+        if(coinToSpin==="heads"){
+            coinHT.innerHTML = "H";
+            array.push("H");
+        }else{
+            coinHT.innerHTML = "T";
+            array.push("T");
+        }
+        kirbyJumping(800);
+        moveCoinBox(800);
+        moveCoin(4000);
+        setTimeout(function(){createCoinTryReminder(array);}, 1100);
+    }else{
+        endGameModal();
+    }
+}
+
+function createCoinTryReminder(array){
+    let reminderSpace = document.getElementsByClassName("reminders");
+    let reminderCoins = document.createElement("div");
+    reminderSpace[0].appendChild(reminderCoins);
+    reminderCoins.setAttribute("class","reminderCoins");
+    reminderCoins.innerHTML = array[array.length-1];
+}
+
+function endGameModal(){
+    let modal = document.getElementsByClassName("endGameModal");
+    modal[0].style.display = "block";
+}
+
+function playAgain(){
+    let endModal = document.getElementsByClassName("endGameModal");
+    if (endModal[0].style.display === "block" && event.target.id === "YesBtn"){
+        location.reload();
+    }
+}
+
+kirbyMotion();
+document.addEventListener("click", function(){
+    flipCoin();
+    playAgain();
+});
